061fe8fe0f929c061183153226ea1d7d27e531d8
chq-johnlukes <johnluke.schaefer@expeditors.com>
2026-05-01 14:50:36 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/timeTracker/",
   plugins: [svelte()],
   resolve: {
     alias: {

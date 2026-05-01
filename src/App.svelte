252b4be32ae564a268af9f7c2eaf4df1824aa05d
chq-johnlukes <johnluke.schaefer@expeditors.com>
2026-05-01 13:08:59 -0700

    saving to localStorage now updates total time

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Timer from './lib/Timer.svelte'
   import TotalTime from './lib/TotalTime.svelte'
-  import { getContext, setContext } from 'svelte';
-
-
 
   
   let ms = 0;
@@ -15,7 +10,6 @@
   let totalH = Math.floor(totalM / 60);
 
   let timerName : string = "";
-  let timerTime : number = 0;
 
   let timers : string [] = [];
   let times : number [] = [];
@@ -24,51 +18,53 @@
 
   const removeTimer = (timer : string) => {
     const index : number = timers.indexOf(timer, 0);
-    console.log(timers);
-    console.log(times);
-    console.log(index);
+    //console.log(timers);
+    //console.log(times);
+    //console.log(index);
     timers.splice(index, 1);
     items.splice(index, 1);
     times.splice(index, 1);
     items = items;
     times = times;
     timers = timers;
-    console.log(timers);
-    console.log(times);
+    //console.log(timers);
+    //console.log(times);
   }
 
   const addItem = () => {
     timers.push(timerName);
     times.push(0);
     timers = timers;
-    console.log(timers);
+    //console.log(timers);
   }
 
   const addExistingTimer = (name: string, ms : number) => {
     timers.push(name);
     times.push(ms);
-    console.log(ms);
+    //console.log(ms);
     timers = timers;
     times = times;
-    console.log(times);
+    //console.log(times);
   }
 
   const saveData = () => {
     localStorage.clear();
     for(let i : number = 0; i < timers.length; i++) {
+      ms += items[i].ms;
       localStorage.setItem(timers[i], "" + items[i].ms);
-      console.log(timers[i] + "," + items[i].ms);
-      console.log(localStorage.getItem(timers[i]));
+      //console.log(timers[i] + "," + items[i].ms);
+      //console.log(localStorage.getItem(timers[i]));
     }
-    console.log(localStorage);
+    //console.log(localStorage);
     
-    console.log(items);
+    updateTimer();
+    //console.log(items);
     timers = timers;
     times = times;
   }
   
   const loadData = () => {
-    console.log(localStorage);
+    //console.log(localStorage);
     let index = 0;
     removeAll();
     
@@ -76,22 +72,22 @@
       if(i === "length") {
         break;
       }
-      console.log(i);
-      console.log(localStorage.getItem(i));
+      //console.log(i);
+      //console.log(localStorage.getItem(i));
       let time : string | null = localStorage.getItem(i);
-      console.log(time);
+      //console.log(time);
       let newMs = 0;
       if(time != null) {
         newMs = parseInt(time, 10);
         ms += newMs;
       }
-      console.log(newMs);
+      //console.log(newMs);
       addExistingTimer(i, newMs);
     }
     
-    console.log(timers);
-    console.log(times);
-    console.log(items);
+    //console.log(timers);
+    //console.log(times);
+    //console.log(items);
   }
 
   const nukeData = () => {
@@ -110,7 +106,7 @@
   }
 
   const updateTimer = () => {
-    console.log("Updating " + ms);
+    //console.log("Updating " + ms);
     totalS = Math.floor((ms / 1000) % 60);
     totalM = Math.floor((totalS / 60) % 60);
     totalH = Math.floor(totalM / 60);
